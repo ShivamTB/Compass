12aa15f6a1a22d5a7946706bb465ed4a2f997f0e
Shafeeque O M <20644896+shafeequeom@users.noreply.github.com>
2024-06-07 23:22:17 +0400

    Initial Setup

---
+++ Client/src/router/routes.ts
@@ -0,0 +1,26 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+  {
+    path: '/',
+    component: () => import('layouts/OuterLayout.vue'),
+    children: [
+      { path: 'register', component: () => import('pages/register/RegisterPage.vue') },
+      { path: 'login', component: () => import('pages/register/LoginPage.vue') }
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
