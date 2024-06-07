caad0e584ff5f388d2afbb4f1014ea8fac9aa735
1010shivam <Nakul1010@gmail.com>
2024-06-07 22:11:02 +0400

    QR code generation and Email

---
+++ registeration/send_email.py
@@ -0,0 +1,50 @@
+import email, smtplib, ssl
+import segno
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+qrcode = segno.make_qr("I will be king of the pirates!")
+qrcode.save("basic_qrcode.png", scale=5, border=0)
+port = 465  # For SSL
+password = "ckmrbvysgxynbvud"
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+subject = "Wecome to Compass!"
+
+sender_email = "compass.strawhats@gmail.com"
+receiver_email = "nakul1010@gmail.com"
+message = "TEST"
+
+message = MIMEMultipart()
+msgAlternative = MIMEMultipart('alternative')
+message.attach(msgAlternative)
+
+msgText = MIMEText('This is the alternative plain text message.')
+msgAlternative.attach(msgText)
+
+# We reference the image in the IMG SRC attribute by the ID we give it below
+msgText = MIMEText('<b>Some <i>HTML</i> text</b> and an image.<br><img src="cid:image1"><br>Nifty!', 'html')
+msgAlternative.attach(msgText)
+
+fp = open('basic_qrcode.png', 'rb')
+msgImage = MIMEImage(fp.read())
+fp.close()
+
+msgImage.add_header('Content-ID', '<image1>')
+# Create a multipart message and set headers
+
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+
+message.attach(msgImage)
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login("compass.strawhats@gmail.com", password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
